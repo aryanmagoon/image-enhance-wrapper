b61630822613730f21d822a5961f5afcb5cac3d6
Aryan Magoon <amagoon757@gmail.com>
2021-12-22 00:22:31 -0500

    fixed model loading, flask, and changed return to json

--- enhance.py
+++ enhance.py
@@ -3,27 +3,25 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 import flask
 from PIL import Image
 import io
-import numpy
+import numpy as np
 import cv2
-import StringIO
+from tempfile import NamedTemporaryFile
+import json
 
 
-upscale=EnhanceAgent(
-    scale=4,
-    model_path="ImageEnhance.pth",
-    model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4),
-    tile=0,
-)
+app=flask.Flask(__name__)
+
 
 @app.route("/enhance", methods=["POST"])
 def enhance():
     if flask.request.method =="POST":
         if flask.request.files.get("image"):
-            image=Image.open(io.BytesIO(flask.request.files("image").read()))
+            image=Image.open(io.BytesIO(flask.request.files["image"].read()))
             image=np.array(image)
+            upscale=EnhanceAgent(scale=4, model_path="ImageEnhance.pth", model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4), tile=0,)
             final=upscale.enhance(image, outscale=3.5)
-            return final
-
+            jsonfinal=json.dumps(final.tolist())
+            return jsonfinal
 
 
 
